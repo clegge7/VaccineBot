e5748499bb207664558681e5431c8bb9699b11c1
clegge7 <colinlegge7@gmail.com>
2021-03-25 20:03:35 -0500

    init
    initial commit

---
+++ HEB_Vaccine_Alert/alerts.js
@@ -0,0 +1,35 @@
+//delay to let site js load list of vaccine appointments
+setTimeout(checkTimeSlots, 1500);
+timeSplotsAvailable = false;
+
+function checkTimeSlots(){
+	//get classes containing distance and time slots available as lists
+	miles = document.getElementsByClassName("evtGGn");
+	timeSlots = document.getElementsByClassName("lhuWlB");
+	
+	//timeSlots will always be <= miles in length so iterate across is to avoid issues
+	for (iter = 0; iter < timeSlots.length; iter++){
+		if(parseFloat(miles[iter].innerHTML.replace(/ .*/,'')) < 30.0 && timeSlots[iter].innerHTML.replace(/ .*/,'') >= 3){
+			timeSplotsAvailable = true;
+			vaccinesAvailable();
+			break;
+		}
+	}
+	if(!timeSplotsAvailable){
+		//refresh page and continue
+		setTimeout(refresh, 3000);
+	}
+}
+
+function vaccinesAvailable(){
+	//change background to bright red and play nuclear siren
+	document.body.style.background = "red";
+	
+	let player = document.createElement("audio");
+	player.src = browser.runtime.getURL('/audio/siren.mp3');
+	player.play();
+}
+
+function refresh(){
+	location.reload();
+}
